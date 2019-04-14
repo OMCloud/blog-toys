e6fc52f70f602478633af48cd3bb43ec2e3b033b
WangYaoSheng <wangyaosheng@zzidc.cn>
2019-04-14 14:14:27 +0800

    修改文章详情的url

--- Blog/urls.py
+++ Blog/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     #使用类视图的url写法
     url(r'^$', views.IndexView.as_view(), name='index'),
 
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
 
     # url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
